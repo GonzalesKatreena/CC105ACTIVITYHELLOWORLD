647aeb48d4ece78764be977fbaae5e591f6097ea
GonzalesKatreena <bg201803131@wmsu.edu.ph>
2020-10-18 11:41:35 +0800

    HelloWorld

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//Gonzales
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello World'),
+      ),
+    ),
+  );
+}
